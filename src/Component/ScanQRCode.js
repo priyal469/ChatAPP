7866025db1eaac0de26cf736390383403af4757e
priyal469 <priyalsharma469@gmail.com>
2021-04-09 14:54:52 +0530

    styles updated

--- src/Component/ScanQRCode.js
+++ src/Component/ScanQRCode.js
@@ -10,24 +10,22 @@ import {
   Button
 } from 'react-native';
 
-
-
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
 import fontFamily from '../styles/fontFamily';
 import colors from '../styles/colors';
 import commonStyles from '../styles/commonStyles';
 
-export function ScanQRCode(props){
-  const{_onSuccess,isCameraOpen}=props;
-    return(
-        <View>
-          {isCameraOpen&&
-          <QRCodeScanner
-        onRead={(e)=>_onSuccess(e)}
-        showMarker={true}
-        // reactivate={true}
-        flashMode={RNCamera.Constants.FlashMode.auto}
+export function ScanQRCode(props) {
+  const { _onSuccess, isCameraOpen } = props;
+  return (
+    <View>
+      {isCameraOpen &&
+        <QRCodeScanner
+          onRead={(e) => _onSuccess(e)}
+          showMarker={true}
+          // reactivate={true}
+          flashMode={RNCamera.Constants.FlashMode.auto}
         // topContent={
         //   <Text style={styles.centerText}>
         //     Go to{' '}
@@ -35,26 +33,26 @@ export function ScanQRCode(props){
         //     your computer and scan the QR code.
         //   </Text>
         // }
-       
-                     />  }
-          
-        </View>
-    )
+
+        />}
+
+    </View>
+  )
 }
 const styles = StyleSheet.create({
   centerText: {
     flex: 1,
-   ...commonStyles.fontSize18,
+    ...commonStyles.fontSize18,
     padding: 32,
-   
+
   },
   textBold: {
-    fontFamily:fontFamily.bold,
+    fontFamily: fontFamily.bold,
     color: colors.white
   },
   buttonText: {
     fontSize: 21,
-    color:colors.themeColor
+    color: colors.themeColor
   },
   buttonTouchable: {
     padding: 16
